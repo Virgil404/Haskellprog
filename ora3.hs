dd20a7b3d87e2831992c10b30bf0f72c8484f67a
Zaccomer Virgil <zaccomervirgil3@gmail.com>
2022-09-30 15:28:30 +0200

    listak

--- ora3.hs
+++ ora3.hs
@@ -1,3 +1,35 @@
-module Lesson3 where 
+module Lesson3 where
+
+--Mintaillesztes
+
+and' :: Bool -> Bool -> Bool
+and' True True = True
+and' _ _ = False
 
 
+first :: (a,b,c)->a 
+first (x,_,_) = x 
+
+listaint :: [Int]
+listaint = [1,2,3,4,5]
+
+listastring :: [String]
+listastring = ["ab","cd","ef", "gh","ii"]
+
+
+listabool :: [Bool]
+listabool = [True ,False,  True, False,True ]
+
+
+--funkciok
+--head
+--length 
+--last 
+--reverse 
+--take 
+--drop
+--sum
+--product
+-- `elem`
+--zip 
+-- !!
