9f3156a324e6a1838967633c38e523ba5992695e
Zaccomer Virgil <zaccomervirgil3@gmail.com>
2022-10-06 15:42:04 +0200

    hazi3 kesz talan

--- hazi3.hs
+++ hazi3.hs
@@ -5,6 +5,12 @@ module Hazi3 where
 
  interval :: Int -> Int -> [Int]
  interval a b = [a..b]
+
+ headTail :: [Int] -> (Int, [Int])
+ headTail  a = (head a, tail a)
  
+ doubleHead :: [Int] -> [Int] -> (Int, Int)
+ doubleHead a b = (head a, head b)
  
- 
+ divFive :: [Int]
+ divFive=[ x | x <- [16..144], even x,x `mod` 5 ==0]
